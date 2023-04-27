2e614a892333807976fd80c47956221f0f11e15f
CLUET <105191189+CLUET-op@users.noreply.github.com>
2023-04-27 08:23:21 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,77 @@
+let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")];
 
+function addTable(table){
+
+    table.table(Tex.pane, t => {
+
+        let s = new Slider(0, 4, 1, false);
+
+        s.setValue(0);
+
+        let l = t.label(() => {
+
+            let v = s.getValue();
+
+            if(v >= 0){
+
+                return "x" + Math.pow(2, v);
+
+            }else{
+
+                return "x1/" + Math.pow(2, Math.abs(v));
+
+            }
+
+        }).growX().width(6.5 * 4.5).color(Pal.accent);
+
+        let b = t.button(new TextureRegionDrawable(Icon.refresh), 24, () => s.setValue(0)).padLeft(6).get();
+
+        b.getStyle().imageUpColor = Pal.accent;
+
+        t.add(s).padLeft(6).minWidth(100);
+
+        s.moved(v => {
+
+            let t = Math.pow(2, v);
+
+            Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * t, 3 * t));
+
+            l.color(Tmp.c1.lerp(cols, (s.getValue() + 8) / 16));
+
+        });
+
+    });
+
+    table.visibility = () => {
+
+        if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
+
+        if(!Vars.mobile) return true;
+
+        
+
+        let input = Vars.control.input;
+
+        return input.lastSchematic == null || input.selectPlans.isEmpty();
+
+    };
+
+}
+
+if(!Vars.headless){
+
+    var tc = new Table();
+
+    Events.on(ClientLoadEvent, () => {
+
+        tc.bottom().left();
+
+        addTable(tc);
+
+        Vars.ui.hudGroup.addChild(tc);
+
+        if(Vars.mobile) tc.moveBy(0, Scl.scl(120));
+
+    });
+
+}
